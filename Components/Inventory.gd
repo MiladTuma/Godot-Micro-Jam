de95579355e391d6ca43f816b44dbd7bcbee49ff
MiladTuma <Miladtuma1@gmail.com>
2026-08-15 22:22:01 +0200

    Fixed bug

--- Components/Inventory.gd
+++ Components/Inventory.gd
@@ -1,17 +1,17 @@
 class_name Inventory
 extends Resource
 
-
+signal inventory_changed
 # Inventory Stuff
-var meat : int = 5
-var berries : int = 0
-var water : int = 5
-var herbs : int = 0
-var spear : int = 1
-var leather : int = 0
-var rope : int = 1
-var stick : int = 2
-var stone : int = 4
+@export var meat : int = 5
+@export var berries : int = 0
+@export var water : int = 5
+@export var herbs : int = 0
+@export var spear : int = 1
+@export var leather : int = 0
+@export var rope : int = 1
+@export var stick : int = 2
+@export var stone : int = 4
 
 var damage_because_of_hunger : int = 20
 var damage_because_of_sickness : int = 5
--- Scene/label.gd
+++ Scene/label.gd
@@ -1,15 +1,16 @@
 extends Label
 
-@export var inventory : Inventory
+@export var inventory: Inventory
 
-func _process(_delta: float) -> void:
+var items = ["meat", "berries", "herbs", "water", "leather", "rope", "stick", "stone", "spear"]
+
+func _ready():
+	inventory.inventory_changed.connect(update_inventory)
+	update_inventory()
+
+
+func update_inventory():
 	text = ""
-	text += "Meat: " + str(inventory.meat) + "\n"
-	text += "Berries: " + str(inventory.berries) + "\n"
-	text += "Herbs: " + str(inventory.herbs) + "\n"
-	text += "Water: " + str(inventory.water) + "\n"
-	text += "Leather: " + str(inventory.leather) + "\n"
-	text += "Rope: " + str(inventory.rope) + "\n"
-	text += "Stick: " + str(inventory.stick) + "\n"
-	text += "Stone: " + str(inventory.stone) + "\n"
-	text += "Spear: " + str(inventory.spear) + "\n"
+
+	for item in items:
+		text += item.capitalize() + ": " + str(inventory.get(item)) + "\n"
