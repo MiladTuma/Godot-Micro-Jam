d88257efe61189c04136124f5f241f09c558e3f4
MiladTuma <Miladtuma1@gmail.com>
2026-08-15 13:27:53 +0200

    Squashed commit of the following:
    commit c301da8084055c49420d52a72e57e22131d835c1
    Author: MiladTuma <Miladtuma1@gmail.com>
    Date:   Sat Aug 15 12:50:48 2026 +0200
    
        Made the Characters into a class
    
    commit ea7b5f8b8b2965cb22d03f9b28aee2a8fdf9f20d
    Author: MiladTuma <Miladtuma1@gmail.com>
    Date:   Sat Aug 15 12:01:35 2026 +0200
    
        Squashed commit of the following:
    
        commit 26180e779c1d4cd4e72b1a7b83d7022d56879c89
        Author: MiladTuma <Miladtuma1@gmail.com>
        Date:   Sat Aug 15 11:58:22 2026 +0200
    
            Revert "Merge branch 'Hp_for_player' into Inventory"
    
            This reverts commit 1cde6192d3d498b7d2b74fd8e393c6b1e7d5fd00, reversing
            changes made to 32e1bf412d4bf33a12e18d50548827fc5c136ecc.
    
        commit 1cde6192d3d498b7d2b74fd8e393c6b1e7d5fd00
        Merge: 32e1bf4 43087bb
        Author: MiladTuma <Miladtuma1@gmail.com>
        Date:   Sat Aug 15 11:54:41 2026 +0200
    
            Merge branch 'Hp_for_player' into Inventory
    
        commit 32e1bf412d4bf33a12e18d50548827fc5c136ecc
        Author: MiladTuma <Miladtuma1@gmail.com>
        Date:   Sat Aug 15 11:12:51 2026 +0200
    
            added crafting
    
        commit 6b451c2c709b286f6349a7a801e2f250ebcab798
        Author: MiladTuma <Miladtuma1@gmail.com>
        Date:   Sat Aug 15 10:45:23 2026 +0200
    
            Added Checkers to see if they have something
    
        commit b85fd2f3e5eca699b0a713696ee35f8020c59b88
        Author: MiladTuma <Miladtuma1@gmail.com>
        Date:   Sat Aug 15 10:13:30 2026 +0200
    
            Added inventory and other stuff
    
    commit 43087bb84d89f111c9d70921bf147dd27237c91e
    Author: MiladTuma <Miladtuma1@gmail.com>
    Date:   Sat Aug 15 11:44:51 2026 +0200
    
        fixed some bugs from nfarl
    
    commit 50a801da3cda3e534e4e3d23c3348c7a15559a47
    Author: NFarl <152657677+NFarl@users.noreply.github.com>
    Date:   Sat Aug 15 11:10:18 2026 +0200
    
        Hp
    
    commit 6d01499a6aed6a85804ef4990cf29ec6b0969262
    Author: NFarl <152657677+NFarl@users.noreply.github.com>
    Date:   Sat Aug 15 10:13:08 2026 +0200
    
        hp

--- Components/Inventory.gd
+++ Components/Inventory.gd
@@ -11,7 +11,7 @@ var spear : int = 1
 var leather : int = 0
 var rope : int = 1
 var stick : int = 2
-var stone : int = 3
+var stone : int = 4
 
 var damage_because_of_hunger : int = 20
 var damage_because_of_sickness : int = 5
